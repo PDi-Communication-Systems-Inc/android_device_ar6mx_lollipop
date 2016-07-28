231f26852623564376b57288b6cd0f7ab798caec
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-07-28 10:14:23 -0400

    Fix typos

--- bcm/ar6mx/ar6mx.mk
+++ bcm/ar6mx/ar6mx.mk
@@ -21,7 +21,7 @@ PRODUCT_COPY_FILES += \
         device/bcm/ar6mx/audio_policy.conf:system/etc/audio_policy.conf \
         device/bcm/ar6mx/audio_effects.conf:system/vendor/etc/audio_effects.conf \
         device/bcm/init.superuser.rc:root/init.superuser.rc \
-        device/bcm/ar6mx/load_wifi_module.sh:system/etc/load_wifi_module.sh \
+        device/bcm/ar6mx/load_wifi_module.sh:system/etc/load_wifi_module.sh
 
 PRODUCT_COPY_FILES +=	\
 	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6d.bin:system/lib/firmware/vpu/vpu_fw_imx6d.bin 	\
@@ -68,7 +68,7 @@ PRODUCT_COPY_FILES += \
 #PDi additions
 PRODUCT_COPY_FILES += \
  	device/bcm/ar6mx/process_ts.sh:system/etc/process_ts.sh \
-	device/bcm/ar6mx/otasetup.sh:system/etc/otasetup.sh
+	device/bcm/ar6mx/otasetup.sh:system/etc/otasetup.sh \
         device/bcm/EETI/eGalaxTouch_VirtualDevice.idc:system/usr/idc/eGalaxTouch_VirtualDevice.idc \
         device/bcm/EETI/eGTouchA.ini:data/eGTouchA.ini \
         device/bcm/EETI/eGTouchD:system/bin/eGTouchD \
