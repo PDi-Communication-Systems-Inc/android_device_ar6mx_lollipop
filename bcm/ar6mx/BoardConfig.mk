da889429a3b5fb99254646bd67195149bb2fdaed
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-07-28 09:39:31 -0400

    use different recovery color space for lvds panels

--- bcm/ar6mx/BoardConfig.mk
+++ bcm/ar6mx/BoardConfig.mk
@@ -169,6 +169,14 @@ BOARD_SEPOLICY_UNION := \
 
 # Other Recovery Options
 TARGET_NO_RECOVERY                      := false
-TARGET_RECOVERY_PIXEL_FORMAT            := "BGRA_8888"
+
+ifeq ($(AIO_CONFIGURATION),F)
+$(warning Non-LVDS panel recovery color space of BGRA_8888) 
+   TARGET_RECOVERY_PIXEL_FORMAT            := "BGRA_8888"
+else
+$(warning LVDS panel recovery color space of RGBA_8888)
+   TARGET_RECOVERY_PIXEL_FORMAT            := "RGBX_8888"
+endif
+
 # TODO: Allow OTA to update bootloader
 #TARGET_RECOVERY_UPDATER_LIBS            := librecovery_updater_ar6mx
