f3204856d7e55a9a0295d6c0bcd58fd3746a8fa9
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-08-18 16:20:24 -0400

    Add script to process bootargs into pdiarm properties, one goal is to capture info of interest from bootloader like the board version

---
+++ bcm/ar6mx/process_cmdline.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# A fairly rough way to separate off the bootargs
+# this might need improvement in the future
+# for now just interested in the board version
+
+CMDLINE=`cat /proc/cmdline`
+echo cmdline=$CMDLINE
+for bootarg in $CMDLINE;
+   do
+      BOOTARG_VALUE=$bootarg
+      echo BOOTARG value is $BOOTARG_VALUE
+      (IFS='=';
+       KEY='' 
+       for kv in $BOOTARG_VALUE;
+          do
+             echo "$kv";
+             if [ -z $KEY]; then
+                KEY=pdiarm.cmdline.$kv
+             else
+                VALUE=$kv
+                setprop $KEY $VALUE
+                break
+             fi
+          done);   
+   done
