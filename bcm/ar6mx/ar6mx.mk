f599dcbc22d394cb1dd73e18517de21632df414f
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-08-31 16:48:41 -0400

    Move superuser init file move to only show up in engineering builds where root is included

--- bcm/ar6mx/ar6mx.mk
+++ bcm/ar6mx/ar6mx.mk
@@ -20,7 +20,6 @@ PRODUCT_COPY_FILES += \
         device/bcm/ar6mx/gpsreset.sh:system/etc/gpsreset.sh \
         device/bcm/ar6mx/audio_policy.conf:system/etc/audio_policy.conf \
         device/bcm/ar6mx/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-        device/bcm/init.superuser.rc:root/init.superuser.rc \
         device/bcm/ar6mx/load_wifi_module.sh:system/etc/load_wifi_module.sh \
         device/bcm/ar6mx/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf
 
@@ -149,6 +148,8 @@ $(warning Engineering build...including Koush superuser package)
    SUPERUSER_PACKAGE := com.bcm.superuser
    SUPERUSER_PACKAGE_PREFIX := .cyanogenmod.superuser
    SUPERUSER_EMBEDDED := true
+
+   PRODUCT_COPY_FILES += device/bcm/init.superuser.rc:root/init.superuser.rc
 else
 $(warning Not an engineering build, not including Koush superuser package)
 endif
