8748c5609fd27bf698c047bb5b79801280062975
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-10-21 12:50:00 -0400

    Duplicate kernel output of scripts into logcat to avoid messy looking output with dmesg as it does not process carriage returns. Note: Process substitution is not supported in Android's default mksh environment, recommendation is to use NamedPipes

--- bcm/ar6mx/otasetup.sh
+++ bcm/ar6mx/otasetup.sh
@@ -1,9 +1,12 @@
 #!/system/bin/sh
+TAG=OTA_SETUP
 
 if [[ -z /data/system/ota.conf ]]
    then
+   log -p i "$TAG" "ota.conf does not exist, copying"
    echo -e "ota.conf does not exist, copying\n"
    cp /system/etc/ota.conf /data/system/ota.conf
 else
+   log -p i "$TAG" "ota.conf already exists"
    echo -e "ota.conf already exists\n"
 fi
