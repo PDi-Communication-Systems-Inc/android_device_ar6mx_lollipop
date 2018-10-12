e3bde644ab4f9187d13e6ea07a9680e33e33be85
sagar pdies2 <sheroorkar@pdies2>
2018-10-12 13:55:01 -0400

    cherry picking 90b8f3201b90498e82eb74d6ccee7ecebc309d92 for internal speaker enable support

--- bcm/ar6mx/ar6mx.mk
+++ bcm/ar6mx/ar6mx.mk
@@ -84,6 +84,7 @@ PRODUCT_COPY_FILES += \
  	device/bcm/ar6mx/process_ts.sh:system/etc/process_ts.sh \
         device/bcm/ar6mx/process_cmdline.sh:system/etc/process_cmdline.sh \
 	device/bcm/ar6mx/otasetup.sh:system/etc/otasetup.sh \
+        device/bcm/ar6mx/initgpios.sh:system/etc/initgpios.sh \
         device/bcm/EETI/eGalaxTouch_VirtualDevice.idc:system/usr/idc/eGalaxTouch_VirtualDevice.idc \
         device/bcm/EETI/eGTouchA.ini:data/eGTouchA.ini \
         device/bcm/EETI/eGTouchD:system/bin/eGTouchD \
@@ -147,12 +148,19 @@ PRODUCT_PACKAGES += AudioRoute							\
 		    PicoTtsLangInstaller  					\
 		    PicoLangInstaller						\
 		    alphavnc                            			\
-  com.pdiarm.pdicore                  \
-                    jniccitvLib                                                 \
-                        pdiarm.com.pditv                                        \
-                        libpdi_directdisp_jni.so
-
-
+		    PDI_mxc-v4l2-tvin                       \
+		    mxc-v4l2-capture  						\
+		    mxc-v4l2-overlay  						\
+		    mxc-v4l2-output  						\
+		    mxc-v4l2-tvin  						\
+		    PicoLangInstaller					\
+                    internalSpeakers                    \
+                    com.pdiarm.pdicore                 	\
+		    pdiarm.com.pditv                    \
+           	    libjniccitvLib.so					\
+		    libpdi_directdisp_jni.so			\
+		    libccitv							\
+		    jniccitvLib
 		    
 
 # for Compat driver
