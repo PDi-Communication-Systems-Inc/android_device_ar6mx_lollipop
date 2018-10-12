1ff951254b9658cd9dd7295bf1e4e3b01010c206
sagar pdies2 <sheroorkar@pdies2>
2018-10-12 13:55:01 -0400

    cherry-pick 90b8f3201b90498e82eb74d6ccee7ecebc309d92 for internal speaker enable

---
+++ bcm/ar6mx/initgpios.sh
@@ -0,0 +1,34 @@
+
+cd /sys/class/gpio
+echo 36 > export
+cd gpio36
+echo high > direction
+
+cd /sys/class/gpio
+echo 37 > export
+cd gpio37
+echo in > direction
+chmod 777 /sys/class/gpio/gpio37/value
+chmod 777 /sys/class/gpio/gpio37/direction
+chown root system /sys/class/gpio/gpio37/value
+chown root system /sys/class/gpio/gpio37/direction
+
+cd /sys/class/gpio
+echo 39 > export
+cd gpio39
+echo out > direction
+chmod 777 /sys/class/gpio/gpio39/value
+chmod 777 /sys/class/gpio/gpio39/direction
+chown root system /sys/class/gpio/gpio39/value
+chown root system /sys/class/gpio/gpio39/direction
+
+cd /sys/class/gpio
+echo 200 > export
+cd gpio200
+echo in > direction
+chmod 777 /sys/class/gpio/gpio200/value
+chmod 777 /sys/class/gpio/gpio200/direction
+chown root system /sys/class/gpio/gpio200/value
+chown root system /sys/class/gpio/gpio200/direction
+
+/system/bin/internalSpeakers
