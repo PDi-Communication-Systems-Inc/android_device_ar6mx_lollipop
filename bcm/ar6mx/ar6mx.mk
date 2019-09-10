16a867b02fc99a6671650675629833d795250c00
sagar pdies2 <sheroorkar@pdies2>
2019-09-10 12:22:44 -0400

    adding eGalax calibration apps to the medtv builds
    (cherry picked from commit bf3cf1a7697bfd22fd43bf32ac136bb0fcdbc425)

--- bcm/ar6mx/ar6mx.mk
+++ bcm/ar6mx/ar6mx.mk
@@ -150,8 +150,11 @@ PRODUCT_PACKAGES += AudioRoute							\
 		    PicoTts							\
 		    PicoTtsLangInstaller  					\
 		    PicoLangInstaller						\
+                    ethernet                                                    \
 		    alphavnc  				                        \
                     internalSpeakers                                            \
+                    com.eeti.android.egalaxupdateauto                           \
+                    com.eeti.android.egalaxsensortester                         \
                     com.pdiarm.managemyaccount
 		    
 
@@ -236,6 +239,9 @@ PRODUCT_PACKAGES += com.pdiarm.managemyaccount \
 		    com.pdiarm.pdicinchwidgets.pdixplain \
 		    pdicinchwidget.apps.android.pdiarm.com.pdicinchwidget \
 		    org.wso2.emm.agent
+
+# Add medTV on standard product medTV branch
+ PRODUCT_PACKAGES += libegalaxapijni.so
 
 PRODUCT_DEFAULT_DEV_CERTIFICATE := \
 				vendor/pdi/security/ar6mx/releasekey
