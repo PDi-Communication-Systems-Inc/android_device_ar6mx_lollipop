08f23fa9f7e4461047efa1dd0c48c3490dcaf0f0
Jon Smith <jtsmith@pdiarm.com>
2021-03-15 17:09:44 -0400

    pdi_ota_script: Put pdi_ota_script service into a secuity context and updated the security policy for it to work.

--- bcm/ar6mx/otasetup.sh
+++ bcm/ar6mx/otasetup.sh
@@ -3,10 +3,10 @@ TAG=OTA_SETUP
 
 if [[ -z /data/system/ota.conf ]]
    then
-   log -p i "$TAG" "ota.conf does not exist, copying"
-   echo -e "ota.conf does not exist, copying\n"
+   log -p i -t "$TAG" "ota.conf does not exist, copying"
+   echo -e "ota.conf does not exist, copying  \n"
    cp /system/etc/ota.conf /data/system/ota.conf
 else
-   log -p i "$TAG" "ota.conf already exists"
-   echo -e "ota.conf already exists\n"
+   log -p i -t "$TAG" "ota.conf already exists"
+   echo -e "ota.conf already exists  \n"
 fi
